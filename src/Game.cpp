5057bb92503723bb929c68280f0bcf4971629104
yugyeomiste <romana.jazz@gmail.com>
2025-12-18 23:10:47 +0100

    Test d'un tour et ajout de commentaires pour pas me perdre

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,5 +1,7 @@
 #include "Game.hpp"
 #include <iostream>
+#include"Player.hpp"
+
 
 
 void init_board(Board& board) {
@@ -23,5 +25,35 @@ void draw_game_board(const Board& board) {
         }
         
         std::cout <<std::endl;
+    }
+}
+
+void play_turn(Board& board, const Player& player) {
+    int choix = 0;
+    bool coupValide = false;
+
+    while (!coupValide) {
+        std::cout << player.name << ", choisis une case (1-9) : ";
+        std::cin >> choix;
+
+        // Entre 1 et 9
+        if (choix < 1 || choix > 9) {
+            std::cout << "[ERREUR] Le chiffre doit etre entre 1 et 9 !" << std::endl;
+            continue; 
+        }
+
+        // Enleve 1 car tableau commence a 0
+        int index = choix - 1; 
+        int ligne = index / 3; 
+        int col = index % 3;   
+
+        // Case indispo
+        if (board[ligne][col] == 'X' || board[ligne][col] == 'O') {
+            std::cout << "[ERREUR] La case est deja prise !" << std::endl;
+        } else {
+            
+            board[ligne][col] = player.symbol;
+            coupValide = true; 
+        }
     }
 }
